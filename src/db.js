b72a70ed72416ea47ad68d6b5b6cb21be5534a9d
rohan-chari <rohan.rchari@gmail.com>
2026-03-12 16:18:31 -0400

    fix

--- src/db.js
+++ src/db.js
@@ -1,18 +1,15 @@
 require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
 const { PrismaPg } = require("@prisma/adapter-pg");
-const { Pool } = require("pg");
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL must be set before initializing Prisma");
 }
 
-const pool = new Pool({
+const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
-
-const adapter = new PrismaPg({ pool });
 
 const prisma = new PrismaClient({ adapter });
 
